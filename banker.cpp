2a17ec3b63468ec574f4573d18c5c4e355680180
CJ Varga <cvarga@localhost.localdomain>
2024-04-26 23:24:58 -0400

    getAlloc and getMAX finished

--- banker.cpp
+++ banker.cpp
@@ -17,12 +17,84 @@ using namespace std;
 
 // get A B or C of resource allocated to a Process
 int getAlloc(int process, char type){
-    return 0;
+    int spaces;
+    // identify how many numbers traversed
+    // to get to right resource
+    if (type == 'A') spaces = 0;
+    if (type == 'B') spaces = 1;
+    if (type == 'C') spaces = 2;
+
+    // return error if process unknown
+    if (process <= -1) return -1;
+    if (process >= 5) return -1;
+    // added number of spaces per line
+    // to get to correct process/row
+    if (process >= 1){
+        spaces += 9;
+        spaces += (6*(process-1));
+    }
+
+    ifstream input("input.txt");
+    string grabber;
+    int counter = 0;
+
+    // traverse numbers until resource reached
+    while (input>>grabber){
+        if (counter == spaces){
+            input.close();
+            return (int)grabber[0] - int('0');
+        }
+        ++counter;
+
+        //prevent infinite loop
+        if (counter >= 200){
+            input.close();
+            return -1;
+        }
+    }
+    input.close();
+    return -1;
 }
 
 // get max A B or C of Process
 int getMax(int process, char type){
-    return 0;
+    int spaces;
+    // identify how many numbers traversed
+    // to get to right resource
+    if (type == 'A') spaces = 3;
+    if (type == 'B') spaces = 4;
+    if (type == 'C') spaces = 5;
+
+    // return error if process unknown
+    if (process <= -1) return -1;
+    if (process >= 5) return -1;
+    // added number of spaces per line
+    // to get to correct process/row
+    if (process >= 1){
+        spaces += 9;
+        spaces += (6*(process-1));
+    }
+
+    ifstream input("input.txt");
+    string grabber;
+    int counter = 0;
+
+    // traverse numbers until resource reached
+    while (input>>grabber){
+        if (counter == spaces){
+            input.close();
+            return (int)grabber[0] - int('0');
+        }
+        ++counter;
+
+        //prevent infinite loop
+        if (counter >= 200){
+            input.close();
+            return -1;
+        }
+    }
+    input.close();
+    return -1;
 }
 
 // get available resources from txt
@@ -43,7 +115,7 @@ int getAvail(char type){
     while (input>>grabber){
         if (counter == spaces){
             input.close();
-            return (int)grabber[0];
+            return (int)grabber[0] - int('0');
         }
         ++counter;
 
@@ -59,6 +131,6 @@ int getAvail(char type){
 
 
 int main(){
-
+    
 }
 
